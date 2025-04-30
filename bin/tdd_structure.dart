f1c9c2ce386a2c154355e64ae8b63ff44bd055b5
Otabek Muradov <2004otabek27@gmail.com>
2025-04-30 14:24:59 +0500

    fix: add help command, and create command

--- bin/tdd_structure.dart
+++ bin/tdd_structure.dart
@@ -4,31 +4,73 @@ import 'package:args/args.dart';
 import 'package:tdd_structure/structure_create.dart';
 
 void main(List<String> arguments) {
-  final parser = ArgParser()
-    ..addOption('name', abbr: 'n', help: 'The name of the structure')
-    ..addOption('feature', abbr: 'f', help: 'The name of the feature to generate')
-    ..addFlag('help', abbr: 'h', negatable: false, help: 'Show usage information');
+  final parser = ArgParser();
 
-  var argResults = parser.parse(arguments);
+  // Add commands
+  final createCommand = ArgParser();
+  final featureCommand = ArgParser()..addOption('name', abbr: 'n', help: 'Feature name', mandatory: true);
 
-  if (argResults['help']) {
-    print('Usage:');
-    print('  Generate core structure: tdd_structure --name <structure_name>');
-    print('  Generate feature: tdd_structure --feature <feature_name>');
-    print(parser.usage);
-    exit(0);
-  }
+  parser
+    ..addCommand('create', createCommand)
+    ..addCommand('feature', featureCommand)
+    ..addFlag('help', abbr: 'h', negatable: false, help: 'Show this help message');
 
-  if (!Directory('lib').existsSync()) {
-    print('Error: You must run this tool in the root of a Flutter project.');
+  try {
+    var argResults = parser.parse(arguments);
+
+    if (argResults['help'] || arguments.isEmpty) {
+      _showHelp(parser);
+      exit(0);
+    }
+
+    if (!Directory('lib').existsSync()) {
+      print('Error: You must run this tool in the root of a Flutter project.');
+      exit(1);
+    }
+
+    if (argResults.command == null) {
+      _showHelp(parser);
+      exit(1);
+    }
+
+    if (argResults.command!.name == 'create') {
+      addCustomStructure('default_structure');
+    } else if (argResults.command!.name == 'feature') {
+      var featureName = argResults.command!['name'];
+      addFeatureStructure(featureName);
+    }
+  } catch (e) {
+    print('Error: ${e.toString()}');
+    _showHelp(parser);
     exit(1);
   }
+}
 
-  if (argResults['feature'] != null) {
-    var featureName = argResults['feature'];
-    addFeatureStructure(featureName);
-  } else {
-    var projectName = argResults['name'] ?? 'default_structure';
-    addCustomStructure(projectName);
-  }
+void _showHelp(ArgParser parser) {
+  print('''
+TDD Structure Generator
+
+A CLI tool for generating TDD-compliant Flutter project structures.
+
+USAGE:
+  tdd_structure <COMMAND> [ARGS]
+
+COMMANDS:
+  create                    Create a new project structure
+  feature --name <feature>  Generate a new feature
+  help                     Show this help message
+
+EXAMPLES:
+  Create a new project:
+    tdd_structure create
+
+  Generate a feature:
+    tdd_structure feature --name user_auth
+    tdd_structure feature -n user_auth
+
+  Show help:
+    tdd_structure help
+    tdd_structure --help
+    tdd_structure -h
+''');
 }
