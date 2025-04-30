e62e727e8a420b0980422e1baa42f7405fade7a3
Otabek Muradov <2004otabek27@gmail.com>
2025-04-30 12:02:45 +0500

    fix: feature layer folder contents

--- lib/structure_create.dart
+++ lib/structure_create.dart
@@ -176,21 +176,161 @@ void addFeatureStructure(String featureName) {
 
   // Create data layer
   createDirectoryAndFile('lib/features/$featureName/data');
+
+  // Create data sources
   createDirectoryAndFile('lib/features/$featureName/data/data_sources');
+  createDirectoryAndFile('lib/features/$featureName/data/data_sources/local_datasource');
+  createDirectoryAndFile('lib/features/$featureName/data/data_sources/remote_datasource');
+
+  // Create data source files
+  createFile(
+    'lib/features/$featureName/data/data_sources/local_datasource/${featureName}_local_datasource.dart',
+    content: '''
+import 'package:tdd_structure/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+class ${featureName.pascalCase}LocalDataSource {
+  // Add your local data source implementation here
+}
+''',
+  );
+
+  createFile(
+    'lib/features/$featureName/data/data_sources/remote_datasource/${featureName}_remote_datasource.dart',
+    content: '''
+import 'package:tdd_structure/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+class ${featureName.pascalCase}RemoteDataSource {
+  // Add your remote data source implementation here
+}
+''',
+  );
+
+  // Create models directory (empty as per requirements)
   createDirectoryAndFile('lib/features/$featureName/data/models');
+
+  // Create repositories
   createDirectoryAndFile('lib/features/$featureName/data/repositories');
+  createFile(
+    'lib/features/$featureName/data/repositories/${featureName}_repository_impl.dart',
+    content: '''
+import 'package:tdd_structure/core/errors/failure.dart';
+import 'package:tdd_structure/features/$featureName/domain/repositories/i_${featureName}_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class ${featureName.pascalCase}RepositoryImpl implements I${featureName.pascalCase}Repository {
+  // Add your repository implementation here
+}
+''',
+  );
 
   // Create domain layer
   createDirectoryAndFile('lib/features/$featureName/domain');
+
+  // Create entities directory (empty as per requirements)
   createDirectoryAndFile('lib/features/$featureName/domain/entities');
+
+  // Create repositories
   createDirectoryAndFile('lib/features/$featureName/domain/repositories');
+  createFile(
+    'lib/features/$featureName/domain/repositories/i_${featureName}_repository.dart',
+    content: '''
+import 'package:tdd_structure/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class I${featureName.pascalCase}Repository {
+  // Add your repository interface here
+}
+''',
+  );
+
+  // Create usecases directory (empty as per requirements)
   createDirectoryAndFile('lib/features/$featureName/domain/usecases');
 
   // Create presentation layer
   createDirectoryAndFile('lib/features/$featureName/presentation');
+
+  // Create bloc
   createDirectoryAndFile('lib/features/$featureName/presentation/bloc');
+  createFile(
+    'lib/features/$featureName/presentation/bloc/${featureName}_bloc.dart',
+    content: '''
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '${featureName}_event.dart';
+import '${featureName}_state.dart';
+
+class ${featureName.pascalCase}Bloc extends Bloc<${featureName.pascalCase}Event, ${featureName.pascalCase}State> {
+  ${featureName.pascalCase}Bloc() : super(${featureName.pascalCase}Initial()) {
+    // Add your event handlers here
+  }
+}
+''',
+  );
+
+  createFile(
+    'lib/features/$featureName/presentation/bloc/${featureName}_event.dart',
+    content: '''
+part of '${featureName}_bloc.dart';
+
+abstract class ${featureName.pascalCase}Event {}
+
+// Add your events here
+class Example${featureName.pascalCase}Event extends ${featureName.pascalCase}Event {}
+''',
+  );
+
+  createFile(
+    'lib/features/$featureName/presentation/bloc/${featureName}_state.dart',
+    content: '''
+part of '${featureName}_bloc.dart';
+
+abstract class ${featureName.pascalCase}State {}
+
+class ${featureName.pascalCase}Initial extends ${featureName.pascalCase}State {}
+// Add your states here
+''',
+  );
+
+  // Create screens
   createDirectoryAndFile('lib/features/$featureName/presentation/screens');
+  createFile(
+    'lib/features/$featureName/presentation/screens/${featureName}_screen.dart',
+    content: '''
+import 'package:flutter/material.dart';
+import '../bloc/${featureName}_bloc.dart';
+
+class ${featureName.pascalCase}Screen extends StatelessWidget {
+  const ${featureName.pascalCase}Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocBuilder<${featureName.pascalCase}Bloc, ${featureName.pascalCase}State>(
+        builder: (context, state) {
+          return const Center(
+            child: Text('${featureName.pascalCase} Screen'),
+          );
+        },
+      ),
+    );
+  }
+}
+''',
+  );
+
+  // Create widgets directory (empty as per requirements)
   createDirectoryAndFile('lib/features/$featureName/presentation/widgets');
 
   print('Feature structure created successfully!');
+}
+
+// Extension to convert string to PascalCase
+extension StringExtension on String {
+  String get pascalCase {
+    if (isEmpty) return this;
+    return split('_')
+        .map((word) => word.isNotEmpty ? word[0].toUpperCase() + word.substring(1).toLowerCase() : '')
+        .join();
+  }
 }
